b8e8b08d4e1192d753a4e768a3fae7a08b9ba4ce
dannamae <dannamaeiso@gmail.com>
2018-05-31 17:05:11 +0800

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,20 @@
 class User < ApplicationRecord
-
-  has_secure_password
   has_secure_token :auth_token
   validates_uniqueness_of :studentnum
+  has_many :attendances
 
+  def invalidate_token
+    self.update_columns(auth_token: nil)
+  end
+
+  def self.valid_login?(studentnum)
+    user = find_by(studentnum: studentnum)
+  end
 
 
   validates :studentnum, presence: true
   validates :firstname, presence: true
-   validates :middlename, presence: true
+  validates :middlename, presence: true
   validates :lastname, presence: true
-  validates :sectionname, presence: true
-  validates :password_digest, presence: true
-
 
 end
