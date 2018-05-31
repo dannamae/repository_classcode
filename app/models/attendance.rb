17273deef6cdfde394129754c5a54049c01818a7
dannamae <dannamaeiso@gmail.com>
2018-05-31 17:04:52 +0800

    attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,11 +1,6 @@
 class Attendance < ApplicationRecord
-
+  belongs_to :user
   validates :date, presence: true
-  validates :time, presence: true
-  validates :studentnum, presence: true
-  validates :firstname, presence: true
-  validates :middlename, presence: true
-  validates :lastname, presence: true
   validates :attendance, presence: true
 
 end
