9424080120602beca4dee690a4b2bae808e7f69f
dannamae <dannamaeiso@gmail.com>
2018-05-31 17:03:14 +0800

    session for attendance added

--- app/controllers/api/v1/session_controller.rb
+++ app/controllers/api/v1/session_controller.rb
@@ -1,21 +1,16 @@
-module Api
-  module V1
-    class SessionController < ApplicationController
- skip_before_action :require_login, only: [:create], raise: :false
-
- def current_user
-   @current_user ||= authentication_auth_token
- end
-
-
+class Api::V1::SessionController < ApplicationController
+ # skip_before_action :require_login, only: [:create], raise: :false
+ #
+ # def current_user
+ #   @current_user ||= authentication_auth_token
+ # end
+ #
  def create
-   if user = User.valid_login?(params[:studentnum], params[:password])
-       user.password_confirmation = params[:password]
-       user.password = params[:password]
+   if user = User.valid_login?(params[:studentnum])
        user.regenerate_auth_token
        render json: {status: 'Sucess', data:user}, status: :ok
    else
-     render_unauthorized("Wrong username or password")
+     render_unauthorized("Student number not found")
    end
 end
 
@@ -24,6 +19,4 @@ end
    current_user.invalidate_token
    head :ok
  end
- end
-end
 end
